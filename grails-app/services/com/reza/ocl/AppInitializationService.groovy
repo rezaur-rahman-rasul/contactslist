48f44487898072f25931ce1aabff02888af5f3cd
rezaur-rahman-rasul <md.rezaur.rahman.rasul@gmail.com>
2023-10-24 18:24:54 +0600

    Commit 5

--- grails-app/services/com/reza/ocl/AppInitializationService.groovy
+++ grails-app/services/com/reza/ocl/AppInitializationService.groovy
@@ -1,11 +1,20 @@
 package com.reza.ocl
 
-import grails.gorm.transactions.Transactional
-
-@Transactional
 class AppInitializationService {
 
-    def serviceMethod() {
+    static initialize() {
+        initMember()
+    }
 
+    private static initMember() {
+        if (Member.count() == 0) {
+            Member member = new Member()
+            member.firstName = "System"
+            member.lastName = "Administrator"
+            member.email = "ocb@domain.com"
+            member.password = "123456"
+            member.memberType = GlobalConfig.USER_TYPE.ADMINISTRATOR
+            member.save(flash: true)
+        }
     }
 }
